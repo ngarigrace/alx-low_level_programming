0aef9626ec05686ee26887487e558100dea72815
ngarigrace <nyambu0105@gmail.com>
2023-07-27 01:53:44 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,13 +16,13 @@ char *cap_string(char *str)
 		{
 			str[i] = str[i] - 32;
 		}
-	}
 	for (j = 0; c[j] != '\0'; j++)
 	{
 		if (c[j] == str[i] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
 		{
 			str[i + 1] = str[i + 1] - 32;
 		}
+	}
 	}
 	return (str);
 }
